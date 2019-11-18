fd7f5311d1dbdd62282c3bbe04826a0db651a663
eva-lm <eva.lm1810@gmail.com>
2019-11-18 17:45:40 +0100

    add select and unselect buttons

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,6 +3,9 @@
 const url = "./assets/services/data.json";
 let rissoto = [];
 
+const selectButton = document.querySelector(".header__button-select");
+const unSelectButton = document.querySelector(".header__button-unselect");
+
 const getData = () => {
   return fetch(url)
     .then(response => response.json())
@@ -30,7 +33,6 @@ const formatData = data => {
   //   console.log(ingredients);
   // }
   console.log("risotittooo", rissoto);
-  console.log("data", data);
   paintIngredients();
 };
 
@@ -54,20 +56,49 @@ const paintIngredients = () => {
   const text = rissoto.map(item => {
     return `<li class="js__item">
     <p class="js__item-ingredients js-item">${item.ingredients}</p>
-    <input type="checkbox" checked />
+    <input class="js-checkbox" type="checkbox"  />
     <p class="js__item-brand js-item">Marca: ${item.brand}</p>
     <p class="js__item-quantity js-item">Cantidad: ${item.quantity}</p>
     <p>Precio: ${item.price}</p>
     <input class="js__item-number" type="number" value="${item.items}" />
     </li>`;
   });
-
   list.innerHTML = text;
 };
 
 const selectIngredients = () => {
-  const selectButton = document.querySelector(".header__button-select");
-  const unSelectButton = document.querySelector(".header__button-unselect");
+  const input = document.querySelectorAll("input");
+  for (let i = 0; i < input.length; i++)
+    if (input[i].type == "checkbox") input[i].checked = 1;
 };
 
+const unSelectIngredients = () => {
+  const input = document.querySelectorAll("input");
+  for (let i = 0; i < input.length; i++)
+    if (input[i].type == "checkbox") input[i].checked = 0;
+};
+
+const addPrice = () => {
+  const elpreciojusto = document.querySelector(".items__total");
+
+  // for (let i = 0; i < rissoto.length; i++) {
+  //   acc += rissoto.price[i];
+  // }
+
+  for (const item of rissoto) {
+    const precio = item.price;
+    console.log(precio);
+    elpreciojusto.innerHTML = "Total: " + precio;
+  }
+  // const yokese = rissoto.map(item => {
+  //   return parseInt(item.price);
+  // });
+
+  // elpreciojusto.innerHTML = "Total: " + yokese + acc;
+  // console.log("precio", yokese);
+};
+
+selectButton.addEventListener("click", selectIngredients);
+unSelectButton.addEventListener("click", unSelectIngredients);
 getData();
+addPrice();
